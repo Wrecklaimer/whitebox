3b30c1e8d5bb6e7b16a8faebab4f79c81d9d80a7
Wrecklaimer <wrecklaimer@gmail.com>
2013-12-28 20:12:38 -0500

    Added post classes to loop.

--- loop.php
+++ loop.php
@@ -6,37 +6,31 @@
 
 <?php wp_reset_query();
 
-if ( have_posts() ) :
+if ( have_posts() ) : ?>
 
-	$i = 0;
-
-	?>
 	<ul class="posts posts-list">
 
 		<?php
-		while ( have_posts() ) :
+		while ( have_posts() ) : the_post(); ?>
 
-			the_post();
-			$i++;
-
-			?>
-			<li class="cf">
+			<li id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?>>
 
 			<?php if ( Whitebox_Settings::get( 'show_post_thumbnails' ) && has_post_thumbnail( $post->ID ) ) { ?>
 				<div class="cover">
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); ?>
+						<?php the_post_thumbnail(); ?>
 					</a>
 				</div><?php
 			} ?>
 
 				<div class="content">
+
 					<h2 class="entry-title">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 					</h2>
 					<span class="entry-meta"><?php
-						_e('By ', THEME_DOMAIN ); the_author_posts_link();
-						_e(' on ', THEME_DOMAIN ); the_date(); echo ' '; the_time(); ?>
+						_e( 'By ', THEME_DOMAIN ); the_author_posts_link();
+						_e( ' on ', THEME_DOMAIN ); the_date(); echo ' '; the_time(); ?>
 					</span>
 
 					<div class="entry-summary">
@@ -45,7 +39,8 @@ if ( have_posts() ) :
 
 				</div> <!-- / .content -->
 
-			</li><?php
+			</li> <!-- / #post-## -->
+			<?php
 
 		endwhile; ?>
 
@@ -53,11 +48,10 @@ if ( have_posts() ) :
 
 	<?php
 
-	get_template_part('pagination');
+	get_template_part('pagination' );
 
 else :
 
-	?><p class="title"><?php _e('There are no posts to display', THEME_DOMAIN); ?></p><?php
+	?><p class="title"><?php _e( 'There are no posts to display', THEME_DOMAIN ); ?></p><?php
 
-endif;
-?>
+endif; ?>
