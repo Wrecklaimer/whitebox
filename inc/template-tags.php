5baf9e7db0bda3192254b76fd530c84ab2d2d141
Wrecklaimer <wrecklaimer@gmail.com>
2014-01-12 17:14:58 -0500

    Changed comment markup; CSS changes.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -123,33 +123,38 @@ function whitebox_comment( $comment, $args, $depth ) {
 	endif;
 	?>
 
-	<div class="comment-author vcard">
-	<?php if ( $args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-	<?php printf( __('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link() ) ?>
-	</div>
+		<div class="comment-avatar">
+		<?php if ( $args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+		</div>
 
-	<?php
-	if ( $comment->comment_approved == '0' ) : ?>
-	<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', THEME_DOMAIN ) ?></em>
-	<br /><?php
-	endif;
-	?>
+		<div class="comment-content">
+			<div class="comment-author vcard">
+			<?php comment_author_link(); ?>
+			</div>
 
-	<div class="comment-meta commentmetadata">
-		<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf( __( '%1$s at %2$s', THEME_DOMAIN ), get_comment_date(),  get_comment_time()) ?></a>
-		<?php edit_comment_link(__( '(Edit)', THEME_DOMAIN ),'  ','' ); ?>
-	</div>
+			<?php
+			if ( $comment->comment_approved == '0' ) : ?>
+			<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', THEME_DOMAIN ) ?></em>
+			<br /><?php
+			endif;
+			?>
 
-	<div class="comment-entry">
-		<?php comment_text() ?>
-	</div>
+			<div class="comment-meta commentmetadata">
+				<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf( __( '%1$s at %2$s', THEME_DOMAIN ), get_comment_date(),  get_comment_time()) ?></a>
+				<?php edit_comment_link(__( '(Edit)', THEME_DOMAIN ),'  ','' ); ?>
+			</div>
 
-	<div class="reply">
-		<?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ) ?>
-	</div>
+			<div class="comment-text">
+				<?php comment_text() ?>
+			</div>
+
+			<div class="reply">
+				<?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ) ?>
+			</div>
+		</div><!-- / comment-content -->
 
 	<?php if ( 'div' != $args['style'] ) : ?>
-	</div><?php
+	</div><!-- / comment-body --><?php
 	endif;
 }
 endif;
