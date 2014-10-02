01681245600c92f7cef0d6b609c4d71bcfa139cb
Wrecklaimer <wrecklaimer@gmail.com>
2014-10-02 19:06:07 -0400

    Framework: copyright_year() now includes all content/post types by default, with optional parameter.

--- inc/framework/components/whitebox-utils.php
+++ inc/framework/components/whitebox-utils.php
@@ -8,12 +8,24 @@ class Whitebox_Utils {
 	/**
 	 * Copyright Year
 	 *
+	 * @param array $post_types Optional. Post types to query.
 	 * @param bool $echo Optional. Output the result.
 	 */
-	public static function copyright_year( $echo = false ) {
-		$args = array( 'numberposts' => '1', 'post_status' => 'publish' );
-		$recent_posts = wp_get_recent_posts( $args );
-		$output = mysql2date( 'Y', $recent_posts[0]['post_date'] );
+	public static function copyright_year( $post_types = array( 'any' ), $echo = false ) {
+		if ( !$post_types )
+			$post_types = array( 'any' );
+
+		$args = array(
+			'post_type' => $post_types,
+			'numberposts' => '1',
+			'post_status' => 'publish'
+		);
+		$query = wp_get_recent_posts( $args );
+
+		if ( count($query) > 0 )
+			$output = mysql2date( 'Y', $query[0]['post_date'] );
+		else
+			$output = '';
 
 		if ( !$echo )
 			return $output;
