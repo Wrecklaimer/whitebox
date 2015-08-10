b16d46fbd843be16b08db3b2a644b0e296b000e8
Kevin Diefenbach <wrecklaimer@gmail.com>
2015-08-10 19:29:53 -0400

    Added one column page template

---
+++ page-templates/page_one-column.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying a page without a sidebar
+ * Template Name: One Column
+ */
+?>
+
+<?php get_header(); ?>
+
+	<?php get_template_part( 'partials/content', 'start' ); ?>
+
+		<div id="main" class="wide" role="main">
+		<?php
+		if ( have_posts() ) :
+			// Start the Loop
+			while ( have_posts() ) : the_post();
+				get_template_part( 'partials/content', 'page' );
+			endwhile;
+		else: ?>
+			<p><?php _e( 'Sorry, no pages matched your criteria.', THEME_DOMAIN ); ?></p>
+		<?php
+		endif; ?>
+		</div> <!-- / #main -->
+
+	<?php get_template_part( 'partials/content', 'end' ); ?>
+
+<?php get_footer(); ?>
